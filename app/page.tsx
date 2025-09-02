2e1efd895a7952b238fcac92fa86203b1eb846dc
Tamim Fatema <106551239+TamimFatema@users.noreply.github.com>
2025-09-02 11:44:14 +0600

    register page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import LoginPage from '@/login/Login'
+import RegisterPage from '@/register/Register'
 import React from 'react'
 
 const page = () => {
   return (
-    <div><LoginPage />
+    <div><RegisterPage />
         </div>
   )
 }
